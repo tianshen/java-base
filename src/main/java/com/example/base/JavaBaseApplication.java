3148009a4a7f7e395a198ff166570f037a826a6b
jiwei <eunhyeyoon@126.com>
2024-01-27 14:44:41 +0800

    提交

--- src/main/java/com/example/base/JavaBaseApplication.java
+++ src/main/java/com/example/base/JavaBaseApplication.java
@@ -8,6 +8,7 @@ public class JavaBaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaBaseApplication.class, args);
+        System.out.println("----------启动");
     }
 
 }
